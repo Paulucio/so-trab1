5bad527e237b0ee517949d6319d3aee521c71734
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2015-10-15 23:38:10 -0300

    Apenas atualizando os arquivos

--- lazyShell.c
+++ lazyShell.c
@@ -1,29 +1,31 @@
 #include "lazyShell.h"
+#include "fila.h"
+#define EVER ;;
 
 void lazyShell(void)
 {
     char *dir;
     char buffer[TAM_BUFFER];
+    // Celula cel = criaCelula();
 
-    // dir = getcwd(0,0);
-
-    //---------cd wait exit
-
-    for(;;)
+    for(EVER)
     {
         dir = getcwd(0,0);
 
         printf("lsh> ~%s $ ",dir);
         // scanf("%s", buffer);
         fgets(buffer,TAM_BUFFER, stdin);
+        // addElemCelula(cel, buffer);
         sscanf(buffer,"%s",buffer);
-
+        //printf("buffer: %s\nteste: %s\n", buffer, teste);
         if(chdir(buffer))
             printf("Diretorio nao encontrado\n");
         // printf("buffer:%s\n", buffer);
-
-
-
     }
+}
 
+void trata_SIGUSR2 (int sig)
+{
+    static int i = 0;
+    printf("Fim do SIGUSR2.\n");
 }
--- lazyShell.h
+++ lazyShell.h
@@ -5,15 +5,13 @@
 #define TAM_BUFFER 50
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <signal.h>
 #include <unistd.h>
 
 void lazyShell(void);
-
-
-
-
+void trata_SIGCTRLC(int);
 
 
 #endif
